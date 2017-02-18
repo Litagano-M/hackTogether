70b711b66e704a88fd27f84c525f6d4ac391768b
Imanuel Sonubi <iman.sonubi@gmail.com>
2017-02-18 08:33:50 -0500

    Add User model and changed URLs a bit

--- hackTogether/urls.py
+++ hackTogether/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^/', include('home.urls')),
-]
+    url(r'^home/', include('home.urls')),
+    url(r'^$', RedirectView.as_view(url='/home/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
